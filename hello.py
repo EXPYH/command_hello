21d60326337d8d1286761a0bdc4591d8f9367f01
expyh <dublineryh@gmail.com>
2016-07-13 19:06:42 +0900

    conflict resolved Github

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
 //For command line git tutorial
 //First : GitHub modification
 //Second : Local repository modification
->>>>>>> origin/master
 print("hello world")
 print("Tell your world")
 print("Tell his world")
